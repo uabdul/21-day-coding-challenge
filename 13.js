23e26667ff5ea13d6982e770c2cfc2083703b3be
Umair Abdul Qadir <umair.abdulq@gmail.com>
2019-05-13 08:59:59 +0000

    Day 13 of 21 Day Coding Challenge

---
+++ 13.js
@@ -0,0 +1,78 @@
+function powerOn() {
+    if (!ship.powerOn) {
+      ship.powerOn = true
+    }
+  }
+  
+  powerOn()
+  
+  function countModules() {
+    return availableModules.length
+  }
+  
+  function countEssential() {
+    essentialModules = availableModules.filter(module => {
+      return module.essential
+    })
+    return essentialModules.length
+  }
+  
+  function loadModule(index) {
+    availableModules[index].enabled = true
+    ship.modules.push(availableModules[index]);
+  }
+  
+  function findModuleIndex(name) {
+    let moduleIndex
+    availableModules.forEach(function(module, index) {
+      if (module.name === name) {
+        moduleIndex = index
+      }
+    })
+    return moduleIndex
+  }
+  loadModule(findModuleIndex("life-support"))
+  loadModule(findModuleIndex("propulsion"))
+  loadModule(findModuleIndex("navigation"))
+  
+  function resetLARRY() {
+    for (let x = 0; x < 10; x++) {
+      LARRY.quack();
+    }
+  }
+  
+  resetLARRY()
+  
+  loadModule(findModuleIndex("communication"))
+  
+  function setMessage() {
+    radio.message = JSON.stringify(navigation)
+  }
+  
+  setMessage()
+  
+  function activateBeacon() {
+    radio.beacon = true
+  }
+  
+  function setFrequency() {
+    radio.frequency = (radio.range.low + radio.range.high) / 2;
+  }
+  
+  function initialize() {
+    navigation.x = 0
+    navigation.y = 0
+    navigation.z = 0
+  }
+  
+  function calibrateX() {
+    for (let x = 1; x <= 12; x++) {
+      signal = checkSignal();
+      if (signal) {
+        navigation.x = signal;
+        break;
+      }
+    }
+  }
+  
+  calibrateX()
